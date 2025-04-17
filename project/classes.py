8a23387f10663e097b1f5e1cc5a18410207a0d9c
Kaswetas <dreaswer@mail.ru>
2025-04-17 15:55:23 +0700

    Добалена коллизия только врагам

--- project/classes.py
+++ project/classes.py
@@ -2,47 +2,55 @@ import pygame
 import time
 from random import randint, choice
 
+from pygame import Vector2
+
+
 class EXPbar(pygame.sprite.Sprite):
-    def __init__(self, x, y, owner):
+    max_exp = 20
+    height = 40
+    level = 0
+    upgrade_hp_list = [1.1, 1.2, 1.1, 1.3, 1.5]
+    upgrade_damage_list = [1.2, 1.15, 1.1, 1.3, 1.5]
+
+    def __init__(self, hero):
         super().__init__()
-        self.x = x
-        self.y = y
-        self.owner = owner
-        self.procent = int((owner.exp / owner.max_exp) * 1400)
-        self.height = 40
+        self.exp = 0
+        self.hero = hero
+        self.procent = None
+
+        self.update()
+
+    def update(self):
+        self.procent = int((self.exp / self.max_exp) * 1200)
         self.image = pygame.Surface((self.procent, self.height))
         self.rect = self.image.get_rect()
-        self.image.fill("YELLOW")
-    
-    def update(self, **kwargs):
-        self.procent = int((self.owner.exp / self.owner.max_exp) * 1200)
-        self.image = pygame.Surface((self.procent, self.height))
-        self.rect = self.image.get_rect()
-        self.image.fill("YELLOW")
-        self.rect.x = self.x
-        self.rect.y = self.y
+        self.image.fill("GOLD")
+        if self.exp >= self.max_exp:
+            self.level += 1
+            self.exp = self.exp % self.max_exp
+            self.max_exp = int(self.max_exp * 1.5)
+
+            self.hero.attack = int(self.hero.attack * 1.3)
+            self.hero.max_hp = int(self.hero.max_hp * 1.2)
+            self.hero.hp = self.hero.max_hp
+
 
 class HPbar(pygame.sprite.Sprite):
     def __init__(self, owner, color, x, y, max_width, height):
         super().__init__()
+        self.procent = None
         self.height = height
         self.max_width = max_width
-        self.x = x
-        self.y = y
+        self.pos = Vector2(x, y)
         self.color = color
         self.owner = owner
-        self.value = owner.rec_hp
-        self.procent = int((self.owner.rec_hp/self.owner.max_hp) * self.max_width)
-        self.image = pygame.Surface((self.procent, self.height))
-        self.rect = self.image.get_rect()
-        self.image.fill(color)
+        self.update()
 
     def check_hp(self):
-        self.procent = int((self.owner.rec_hp/self.owner.max_hp) * self.max_width)
-        if self.procent < 0:
-            self.procent = 0
+        self.procent = int((self.owner.hp / self.owner.max_hp) * self.max_width)
+        self.procent = (abs(self.procent) + self.procent) / 2
 
-    def update(self, **kwargs):
+    def update(self):
         self.check_hp()
         self.image = pygame.Surface((self.procent, self.height))
         self.rect = self.image.get_rect()
@@ -52,47 +60,39 @@ class HPbar(pygame.sprite.Sprite):
 
 
 class AttackArea(pygame.sprite.Sprite):
+    width = 170
+    height = 170
+
     def __init__(self, owner, attack_type):
         super().__init__()
         self.attack_type = attack_type
         self.color = "GREY"
         self.owner = owner
-        self.width = 170
-        self.height = 170
         self.image = pygame.Surface((self.width, self.height))
         self.rect = self.image.get_rect()
         self.image.fill(self.color)
         self.image.set_alpha(120)
         self.last_pressed_attack = time.perf_counter()
 
+    def change_color(self):
+        if time.perf_counter() - self.owner.last_pressed_attack >= self.owner.cd_attack:
+            self.image.fill("GREY")
+        else:
+            self.image.fill("BLACK")
+
     def attack(self):
-        if self.attack_type == "player":
-            keys = pygame.key.get_pressed()
-            if time.perf_counter() - self.last_pressed_attack >= self.owner.cd_attack:
-                self.image.fill("GREY")
-                if keys[pygame.K_1]:
-                    for el in self.owner.enemy_list:
-                        if self.rect.colliderect(el):
-                            el.rec_hp -= self.owner.attack
-                            el.who_attacked = self.owner
-                        
-                        if el.rec_hp <= 0:
-                            el.get_exp(self.owner)
-                    self.last_pressed_attack = time.perf_counter()
-            else:
-                self.image.fill("BLACK")
-        
         if self.attack_type == "auto":
-            if self.owner.who_attacked is not None and time.perf_counter() - self.last_pressed_attack >= self.owner.cd_attack and self.rect.colliderect(self.owner.who_attacked.rect):
-                self.owner.who_attacked.rec_hp -= self.owner.attack
-                        
+            if self.owner.who_attacked is not None and time.perf_counter() - self.last_pressed_attack >= self.owner.cd_attack and self.rect.colliderect(
+                    self.owner.who_attacked.rect):
+                self.owner.who_attacked.hp -= self.owner.attack
+
                 self.last_pressed_attack = time.perf_counter()
-    
+
     def death(self):
-        if self.owner.rec_hp <= 0:
+        if self.owner.hp <= 0:
             self.kill()
 
-    def update(self, **kwargs):
+    def update(self):
         self.rect.center = self.owner.rect.center
         self.attack()
         self.death()
@@ -136,44 +136,44 @@ class Inventory(pygame.sprite.Sprite):
                 y += 70
             x += 70
             self.image.blit(self.items[i].image, (x, y))
+
+
 class Hero(pygame.sprite.Sprite):
+    width = 80
+    height = 80
+    color = 'WHITE'
+
     def __init__(self, x, y, enemy_list):
         super().__init__()
-        self.inventory = Inventory(self)
-        self.x = x
-        self.y = y
-        self.cd_attack = 1
-        self.level = 0
-        self.points = 0
-        self.exp = 0
-        self.max_exp = 20
-        self.attack = 60
-        self.enemy_list = enemy_list
-        self.width = 80
-        self.height = 80
-        self.color = "WHITE"
-        self.image = pygame.Surface((self.width, self.height))
+
+        self.image = pygame.Surface((Hero.width, Hero.height))
         self.rect = self.image.get_rect()
         self.image.fill(self.color)
-        self.speed = 5
-        self.max_hp = 100
-        self.rec_hp = 100
         self.rect.move_ip(x, y)
-        self.exp_bar = EXPbar(0, 0, self)
-        self.hp_bar = HPbar(self, "GREEN", 0, self.x, 80, 20)
+        self.camera = Vector2(260, -40)
+
+        self.pos = Vector2(x, y)
+        self.speed = 5
+
+        self.cd_attack = 1
+        self.attack = 60
         self.attack_area = AttackArea(self, "player")
-        self.camera = [260, -40]
+
+        self.max_hp = 100
+        self.hp = self.max_hp
+        self.hp_bar = HPbar(self, "GREEN", 0, self.pos.x, 80, 20)
+
+        self.exp_bar = EXPbar(self)
+
+        self.enemy_list = enemy_list
+
+        self.inventory = Inventory(self)
+
+        self.last_pressed_attack = time.perf_counter()
     
     def death(self):
-        if self.rec_hp <= 0:
+        if self.hp <= 0:
             self.kill()
-
-    def exp_check(self):
-        if self.exp >= self.max_exp:
-            self.level += 1
-            self.points += 1
-            self.exp = self.exp % self.max_exp
-            self.max_exp = int(self.max_exp * 1.5)
 
     def move(self):
         keys = pygame.key.get_pressed()
@@ -191,14 +191,24 @@ class Hero(pygame.sprite.Sprite):
             self.camera[1] -= self.speed
         
     def update(self, **kwargs):
-        self.x = self.rect.x
-        self.y = self.rect.y
-        self.attack_area.update()
-        self.exp_check()
+        self.pos.x, self.pos.y = self.rect.x, self.rect.y
+        self.attack_event()
         self.move()
-        self.hp_bar.update()
-        self.exp_bar.update()
         self.death()
+
+    def attack_event(self):
+        self.attack_area.change_color()
+        if time.perf_counter() - self.last_pressed_attack >= self.cd_attack:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_1]:
+                for el in self.enemy_list:
+                    if self.attack_area.rect.colliderect(el):
+                        el.hp -= self.attack
+                        el.who_attacked = self
+                    if el.hp <= 0:
+                        el.get_exp()
+                self.last_pressed_attack = time.perf_counter()
+
 
 class Spawner(pygame.sprite.Sprite):
     def __init__(self, x, y, enemy_cnt, enemyes, sprites, enemy_attack, enemy_exp, enemy_speed, enemy_hp, items_chance):
@@ -239,6 +249,7 @@ class Spawner(pygame.sprite.Sprite):
         self.rect.y = self.y
         self.check_spawn()
 
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, spawner, x, y, attack, exp, speed, hp, items_chance:dict):
         super().__init__()
@@ -260,33 +271,28 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.x = self.x
         self.rect.y = self.y
         self.max_hp = hp
-        self.rec_hp = self.max_hp
+        self.hp = self.max_hp
         self.hp_bar = HPbar(self, "Brown", x, y - 40 , 80, 20)
         self.attack_area = AttackArea(self, "auto")
 
-    def get_exp(self, hero):
-        hero.exp += self.give_exp
+    def get_exp(self):
+        self.who_attacked.exp_bar.exp += self.give_exp
 
     def follow_and_check(self):
-        if self.who_attacked is not None:
-            if self.who_attacked.rect.x > self.rect.x:
-                self.rect.x += self.speed
-            elif self.who_attacked.rect.x < self.rect.x:
-                self.rect.x -= self.speed
-            
-            if self.who_attacked.rect.y > self.rect.y:
-                self.rect.y += self.speed
-            elif self.who_attacked.rect.y < self.rect.y:
-                self.rect.y -= self.speed
-        
+        if self.who_attacked is not None and not self.rect.colliderect(self.who_attacked.rect):
+            dirvect = pygame.math.Vector2(self.who_attacked.rect.x - self.rect.x,
+                                         self.who_attacked.rect.y - self.rect.y)
+            dirvect.normalize()
+            dirvect.scale_to_length(self.speed)
+            self.rect.move_ip(dirvect)
         distance = ((self.rect.x - self.spawner.rect.center[0])**2 + (self.rect.y - self.spawner.rect.center[1])**2) ** 0.5
         if distance > 400:
             self.rect.x, self.rect.y = self.spawner.rect.center[0], self.spawner.rect.center[1]
             self.who_attacked = None
-            self.rec_hp = self.max_hp
+            self.hp = self.max_hp
 
     def death(self):
-        if self.rec_hp <= 0:
+        if self.hp <= 0:
             rec_item = choice(list(self.items_chance.keys()))
             if rec_item not in self.who_attacked.inventory.items and randint(1, 100) <= self.items_chance[rec_item]:
                 self.who_attacked.inventory.items.append(rec_item)
